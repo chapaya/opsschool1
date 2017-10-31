5574942183331dc3dc316ab8c6a8fcf9b1046785
Eran G <egrimberg@gmail.com>
2017-10-31 14:31:43 +0200

    Update exercise5-fix_server1.sh

--- vagrant/fixScripts/exercise5-fix_server1.sh
+++ vagrant/fixScripts/exercise5-fix_server1.sh
@@ -2,8 +2,9 @@
 #add fix to exercise5-server1 here
 
 
-# Need to generate authentication keys and take the public key form one sever and append it to authorized_keys
+# Need to generate authentication keys and take the public key from one sever and append it to authorized_keys in second server
+
 
 sudo ssh-keygen -t rsa
 
-cat .ssh/id_rsa.pub | ssh server2 'cat >> .ssh/authorized_keys'
+cat .ssh/id_rsa.pub | ssh server1 'cat >> .ssh/authorized_keys'
